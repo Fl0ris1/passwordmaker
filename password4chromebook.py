f9f961cf8f7f1f072225fde075884a7de5deffe4
Fl0ris1 <149056628+Fl0ris1@users.noreply.github.com>
2025-04-15 10:16:17 +0200

    Create password4chromebook.py

---
+++ password4chromebook.py
@@ -0,0 +1,29 @@
+from tkinter import *
+import os
+from tkinter.font import Font
+from pswd_maker import passwordmaker
+
+os.system("clear")
+def show():
+    os.system("clear")
+    passw=passwordmaker()
+    password_lbl.config(text=f"     PASSWORD:    {passw}")
+    print(f"PASSWORD: {passw}")
+
+root=Tk()
+
+font1=Font(family="consolas",weight="bold")
+root.geometry("500x250")
+root.config(background="#7FC6A4")
+root.title("Password Generator")
+
+head_lbl=Label(root,text="PASSWORD GENERATOR",font=(font1,13),fg="#454B66",bg="#7FC6A4")
+head_lbl.place(x=0,y=0)
+
+gen_button=Button(root,text="GENERATE!",font=(font1,13),fg="#454B66",bg="#88A09E",command=show)
+gen_button.place(x=200,y=150)
+
+password_lbl=Label(root,text="Press The Button To Generate A Password",font=(font1,13),fg="#454B66",bg="#7FC6A4")
+password_lbl.place(x=100,y=80)
+
+root.mainloop()
